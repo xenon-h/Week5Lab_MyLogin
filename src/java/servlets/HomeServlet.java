1ee9fb3fc3d2a697491e28035f9546cb21ab38bb
759005 <759005@ELF8IT221540.ACDM.DS.SAIT.CA>
2020-10-13 00:10:15 -0600

    Lab 5 Start on home page and redirect to login if not currently logged in

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -7,9 +7,7 @@ package servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 /**
  *
@@ -20,7 +18,13 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") == null) {
+            response.sendRedirect("login");
+        } else {
+
+            getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+        }
     }
 
     @Override
